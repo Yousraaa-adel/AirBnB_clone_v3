8f4900d076ff6c16db66adbce4cf3f2a04089eb5
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-12 23:04:53 +0300

    Try 2 Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,4 +13,4 @@ def teardown(exception):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    app.run(host="0.0.0.0", port=5050, threaded=True)
