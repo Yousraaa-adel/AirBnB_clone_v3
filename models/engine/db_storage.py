347b5d3b164f8d869e0859f0ca5feba1f5c09cd8
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-12 16:00:10 +0300

    Task 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,7 +17,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+        "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class DBStorage:
@@ -33,10 +33,10 @@ class DBStorage:
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(HBNB_MYSQL_USER,
-                                             HBNB_MYSQL_PWD,
-                                             HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB))
+                                    format(HBNB_MYSQL_USER,
+                                            HBNB_MYSQL_PWD,
+                                            HBNB_MYSQL_HOST,
+                                            HBNB_MYSQL_DB))
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
@@ -74,3 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ """
+        
---
+++ test.py
@@ -0,0 +1,8 @@
+dict1 = {"place", "city", "state"}
+
+cls = "city"
+
+if cls in dict1:
+    print(cls)
+else:
+    print("Fail")
