7f612627285d7fe5df9d8a04834d18d29cde4619
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-12 23:11:00 +0300

    Try 4 Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,4 +14,11 @@ def teardown(exception):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050, threaded=True)
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
