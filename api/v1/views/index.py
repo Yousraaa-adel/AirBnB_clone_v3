f26882b8e29bbc6477b3e112e44bbf95be225039
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-13 23:25:27 +0300

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,31 @@
 # api/v1/views/index.py
 from flask import jsonify
 from api.v1.views import app_views
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models import storage
+
 
 @app_views.route("/status", methods=["GET"])
 def get_status():
     return {"status": "OK"}
+
+
+@app_views.route("/stats", methods=["GET"])
+def get_stats():
+    # classes = [Amenity, User, City, Review, State, Place]
+    # strings = ["amenities", "users", "cities", "reviwes", "states", "places"]
+
+    classes = {"amenities": Amenity, "cities": City,
+        "places": Place, "reviwes": Review, "states": State, "users": User}
+    dict1 = {}
+
+    for key, value in classes.items():
+        x = storage.count(value)
+        dict1[key] = x
+
+    return dict1
