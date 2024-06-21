57694b67e050d0e3d5b28fd1aeaf5f1ecdaea6f9
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-21 23:35:52 +0300

    State

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,7 @@
 """
 # api/v1/views/__init__.py
 from flask import Blueprint
+# from api.v1.views import states
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
@@ -10,3 +11,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *
+from api.v1.views.states import *
