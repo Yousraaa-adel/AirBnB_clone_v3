0a8664db52e42b26c756f50d9a36bd4b681fa041
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-21 20:32:59 +0300

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,9 +2,12 @@
 """_summary_
 """
 from flask import Flask
+from flask import make_response
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 from os import environ
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,6 +16,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
