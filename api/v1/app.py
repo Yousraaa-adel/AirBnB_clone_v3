aa26f7fc3c21fc283c8d90919840cca03bceb1f1
Yassin9323 <yassin.waleed94@gmail.com>
2024-06-23 21:54:44 +0300

    try num 16

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from os import environ
 
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+# app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
